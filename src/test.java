02b35613e582dd107fb31266b53cb8845cd1521e
AhmadNader319 <anader3194747@gmail.com>
2022-11-10 19:24:24 +0200

    more clarification_2

---
+++ src/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        Student student = new Student(111223333, 1970, 5, 3);
+        BirthDate date = student.getBirthDate();
+        date.setYear(2010); // Now the student birth year is changed!
+    }
+}
